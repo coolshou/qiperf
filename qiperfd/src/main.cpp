1760868df5d3307630278e068fa41a24eb062848
coolshou <coolshou2060@gmail.com>
2022-12-20 16:30:55 +0800

    add app name/domain

--- qiperfd/src/main.cpp
+++ qiperfd/src/main.cpp
@@ -1,11 +1,14 @@
 #include <QCoreApplication>
 
 #include "qiperfd.h"
+#include "../src/comm.h"
 
 int main(int argc, char *argv[])
 {
     QCoreApplication app(argc, argv);
+    app.setOrganizationName(QIPERF_ORG);
+    app.setOrganizationDomain(QIPERF_DOMAIN);
+    app.setApplicationName(QIPERFD_NAME);
     QIperfd qiperfd = QIperfd();
-
     return app.exec();
 }
