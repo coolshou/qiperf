9bec4d3822b036b4150cd8cf77fad489791568c0
coolshou <coolshou2060@gmail.com>
2022-12-20 11:18:11 +0800

    restructure project

---
+++ src/comm.h
@@ -0,0 +1,10 @@
+#ifndef COMM_H
+#define COMM_H
+
+#define QIPERFD_NAME  "qiperfd"
+
+#define CMD_OK     "OK"
+#define CMD_FAIL   "FAIL"
+#define CMD_STATUS "status"
+
+#endif // COMM_H
--- src/qiperf.h
+++
@@ -1,8 +0,0 @@
-#ifndef QIPERF_H
-#define QIPERF_H
-
-#define APP_ORG  "coolshou"
-#define APP_DOMAIN  "coolshou.idv.tw"
-#define APP_NAME  "qiperf"
-
-#endif // QIPERF_H
