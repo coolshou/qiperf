6e714c86d098bc909e8d6821667fd279d1fef42a
coolshou <coolshou2060@gmail.com>
2022-12-20 16:31:06 +0800

    add comm define

--- src/comm.h
+++ src/comm.h
@@ -1,7 +1,12 @@
 #ifndef COMM_H
 #define COMM_H
 
+#define QIPERF_DOMAIN "coolshou.idv"
+#define QIPERF_ORG "coolshou"
 #define QIPERFD_NAME  "qiperfd"
+#define QIPERFD_PORT 47014
+
+#define QIPERFC_NAME  "qiperfconsole"
 
 #define CMD_OK     "OK"
 #define CMD_FAIL   "FAIL"
