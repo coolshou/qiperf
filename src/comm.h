95abde9df3fdd7e8b151c3edd6ed804c7f013458
coolshou <coolshou2060@gmail.com>
2023-02-14 18:43:59 +0800

    add QIPERFTRAY_NAME

--- src/comm.h
+++ src/comm.h
@@ -4,6 +4,7 @@
 #define QIPERF_DOMAIN "alphanetworks.com"
 #define QIPERF_ORG "alphanetworks"
 #define QIPERFD_NAME  "qiperfd"
+#define QIPERFTRAY_NAME  "qiperftray"
 #define QIPERFD_PORT 47014
 #define RPC_PORT 57025
 
@@ -13,8 +14,10 @@
 #define CMD_FAIL   "FAIL"
 
 #define CMD_STATUS "status"
-#define CMD_IPERF_START  "START"
-#define CMD_IPERF_STOP   "STOP"
+#define CMD_IFNAMES "IFNAMES"  //Get all interfaces
+//iperf control
+#define CMD_IPERF_START  "IPERF_START"
+#define CMD_IPERF_STOP   "IPERF_STOP"
 #define CMD_IPERF_ADD "IPERF_ADD"
 
 #endif // COMM_H
