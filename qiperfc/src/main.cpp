fd279eb89aa2947f0c36fe0da03b3fde7a57f6db
coolshou <coolshou2060@gmail.com>
2023-10-11 15:35:26 +0800

    add myMessageOutput()

--- qiperfc/src/main.cpp
+++ qiperfc/src/main.cpp
@@ -3,8 +3,35 @@
 #include <QApplication>
 #include "../src/comm.h"
 
+void myMessageOutput(QtMsgType type, const QMessageLogContext &context, const QString &msg)
+{
+    QByteArray localMsg = msg.toLocal8Bit();
+    const char *file = context.file ? context.file : "";
+    const char *function = context.function ? context.function : "";
+    switch (type) {
+    case QtDebugMsg:
+        fprintf(stderr, "DEBUG: %s (%s:%u, %s)\n", localMsg.constData(), file, context.line, function);
+        break;
+    case QtInfoMsg:
+        fprintf(stderr, "INFO: %s (%s:%u, %s)\n", localMsg.constData(), file, context.line, function);
+        break;
+    case QtWarningMsg:
+        fprintf(stderr, "WARN: %s (%s:%u, %s)\n", localMsg.constData(), file, context.line, function);
+        break;
+    case QtCriticalMsg:
+        fprintf(stderr, "CRITICAL: %s (%s:%u, %s)\n", localMsg.constData(), file, context.line, function);
+        break;
+    case QtFatalMsg:
+        fprintf(stderr, "FATAL: %s (%s:%u, %s)\n", localMsg.constData(), file, context.line, function);
+        break;
+    }
+}
+
 int main(int argc, char *argv[])
 {
+    //log file
+
+    qInstallMessageHandler(myMessageOutput);
     QApplication app(argc, argv);
     app.setOrganizationName(QIPERF_ORG);
     app.setOrganizationDomain(QIPERF_DOMAIN);
