cfe394976e0fb6a6db61092d33b71e170849568b
coolshou <coolshou2060@gmail.com>
2023-11-01 11:41:46 +0800

    bump to 0.2.11211.01

--- qiperfd/src/version.h
+++ qiperfd/src/version.h
@@ -3,10 +3,6 @@
 
 #include "../src/versions.h"
 
-//#define VERSION_MAJOR 0
-//#define VERSION_MINOR 1
-//#define VERSION_PATCH 202305
-//#define VERSION_BUILD 29
 
 #define VERSION QIPERFD_VERSION
 
