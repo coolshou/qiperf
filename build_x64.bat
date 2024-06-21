6b1f78e23eb9f55629f808aab596960ac2eaf011
coolshou <coolshou2060@gmail.com>
2024-06-21 08:54:17 +0800

    update path

--- build_x64.bat
+++ build_x64.bat
@@ -1,9 +1,9 @@
 @ECHO OFF
 
 set PATH=D:\Qt\5.15.2\mingw81_64\bin;D:\Qt\Tools\mingw810_64\bin;C:\msys64\usr\bin;C:\msys64\mingw64\bin;PATH
-set PATH=C:\Program Files (x86)\NSIS\;PATH
 make distclean
 qmake
 make
 
-makensis.exe /V3 qiperf.nsi
+set PATH=C:\Program Files (x86)\NSIS\;PATH
+makensis.exe /DWIN64 /V3 qiperf.nsi
