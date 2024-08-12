c6e21f1513deae9f248019c73bd1436a87385526
coolshou <coolshou2060@gmail.com>
2024-08-12 13:37:28 +0800

    code cleanup

--- qiperfc/src/wsclient.cpp
+++ qiperfc/src/wsclient.cpp
@@ -220,7 +220,6 @@ void WSClient::onBinaryMessageReceived(const QByteArray &message) {
         }
         m_files[from]->write(message.mid(nullIndex + 1));
     } else {
-        qDebug() << "onBinaryMessageReceived: m_files "<< m_files[from] << "exist: write: " << message;
         m_files[from]->write(message);
     }
 
