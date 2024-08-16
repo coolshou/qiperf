a42fca361c29812e9827924b5d96d352e19cd84e
coolshou <coolshou2060@gmail.com>
2024-08-16 17:17:24 +0800

    add comment

--- qiperfd/src/myservice.cpp
+++ qiperfd/src/myservice.cpp
@@ -2,6 +2,39 @@
 #include <QSysInfo>
 
 #include <QDebug>
+/*windows get service status
+ sc query qiperfd
+SERVICE_NAME: qiperfd
+        TYPE               : 10  WIN32_OWN_PROCESS
+        STATE              : 4  RUNNING
+                                (STOPPABLE, NOT_PAUSABLE, ACCEPTS_SHUTDOWN)
+        WIN32_EXIT_CODE    : 0  (0x0)
+        SERVICE_EXIT_CODE  : 0  (0x0)
+        CHECKPOINT         : 0x0
+        WAIT_HINT          : 0x0
+ sc start qiperfd
+ sc stop qiperfd
+ ==>TODO sc restart
+
+*/
+/* Linux get serive status
+systemctl status qiperfd
+
+qiperfd.service - quick iperf daemon service
+     Loaded: loaded (/usr/lib/systemd/system/qiperfd.service; enabled; vendor preset: enabled)
+     Active: active (running) since Thu 2024-08-08 08:58:34 CST; 4 days ago
+   Main PID: 1370 (qiperfd)
+      Tasks: 1 (limit: 18654)
+     Memory: 2.2M
+        CPU: 10.429s
+     CGroup: /system.slice/qiperfd.service
+             └─1370 /opt/qiperf/bin/qiperfd
+
+systemctl start qiperfd
+systemctl stop qiperfd
+systemctl restart qiperfd
+*/
+
 
 MyService::MyService(QIperfd *qiperfd, QObject *parent)
     : QObject{parent}
