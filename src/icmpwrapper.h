00191ddb066c3ebecdcb14f9429ea8e4aaa094cc
coolshou <coolshou2060@gmail.com>
2024-08-26 10:20:06 +0800

    force _WIN32_WINNT 0x0601

--- src/icmpwrapper.h
+++ src/icmpwrapper.h
@@ -16,9 +16,9 @@
 #include <getopt.h>
 
 #ifdef _WIN32
-#ifndef _WIN32_WINNT
+//#ifndef _WIN32_WINNT
     #define _WIN32_WINNT 0x0601 /* for inet_XtoY functions on MinGW */
-#endif
+//#endif
 
 #include <process.h>  /* _getpid() */
 #include <winsock2.h>
