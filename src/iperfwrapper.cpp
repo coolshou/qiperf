74356c18537d922a50e337edb8f9437b2f275285
coolshou <coolshou2060@gmail.com>
2024-09-23 09:00:39 +0800

    comment

--- src/iperfwrapper.cpp
+++ src/iperfwrapper.cpp
@@ -124,12 +124,9 @@ void IperfWrapper::parserIperf3(QString linedata)
             linedata.contains("Reverse mode")){
             //ignore this lines
         }else if(linedata.contains("iperf Done.")){
-            // TODO: when see this : iperf server run finish!!
+            // when see this : iperf server run finish!!
         }else if(linedata.contains("sender")){
-    //             ||
-    //             linedata.contains("receiver")){
-            //TODO: final sumary
-
+            // ignore sender
         }else{
     //        qDebug() << "parserIperf3: " << linedata;
             QString sDir = nullptr;
