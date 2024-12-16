a3cae9d68479f53f1008337305d637a47ad55c77
coolshou <jimmy_yeh@alphanetworks.com>
2024-12-16 15:35:43 +0800

    add checkFirewallStatus() for windows

--- qiperfd/src/qiperfd.h
+++ qiperfd/src/qiperfd.h
@@ -88,6 +88,7 @@ private slots:
     void onWSactMessage(QString msg); //procress websocket action message
     void onNewClient(QHostAddress addr); //
 private:
+    void checkFirewallStatus();
     QString tmppath;
     QString qiperfdlog;
     QString tmpfilepath;
