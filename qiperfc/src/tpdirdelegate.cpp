08b6e53dfc6b67ab87b26445702ed2c6a0525e36
coolshou <jimmy_yeh@alphanetworks.com>
2024-12-19 11:35:14 +0800

    fix not show text on throughput Direction Column

--- qiperfc/src/tpdirdelegate.cpp
+++ qiperfc/src/tpdirdelegate.cpp
@@ -10,21 +10,67 @@ TPDirDelegate::TPDirDelegate(QObject *parent)
 void TPDirDelegate::paint(QPainter *painter, const QStyleOptionViewItem &option, const QModelIndex &index) const
 {
     if (index.isValid() && index.column() == TP::cols::dir) {
-        QModelIndex itm;// = index;
-        QStyledItemDelegate::paint(painter, option, itm); //do not draw text
-        // Assuming you want to display images in column::dir
+        QStyleOptionViewItem opt = option;
+        initStyleOption(&opt, index);
+        // Custom painting for the background and selection highlights
+        painter->save();
+
+        // Draw background
+        if (opt.state & QStyle::State_Selected) {
+            painter->fillRect(opt.rect, opt.palette.highlight());
+        } else {
+            painter->fillRect(opt.rect, opt.palette.base());
+        }
+
+        // Draw custom decorations (e.g., icons, borders, etc.)
+        if (!opt.icon.isNull()) {
+            QRect iconRect = QStyle::alignedRect(
+                opt.direction,
+                Qt::AlignLeft,
+                opt.icon.actualSize(opt.decorationSize),
+                opt.rect
+                );
+            opt.icon.paint(painter, iconRect, Qt::AlignLeft);
+        }
         QVariant data = index.data(); // Retrieve data associated with the index
         if (data.isValid()) {
             QString imagePath = ":/"+data.toString();// Assumes you store the image path in Qt::UserRole
             QImage image(imagePath);
             if (!image.isNull()) {
                 QPixmap pixmap = QPixmap::fromImage(image);
-                painter->drawPixmap(option.rect, pixmap);
-            }else{
-//                qDebug() << "No image: " << imagePath << ::Qt::endl;
+                QRect rect = option.rect;
+                // rect.setWidth((option.rect.width()/3)*2); // adjust size
+                painter->drawPixmap(rect, pixmap);
             }
-        } else {
-            qDebug() << "data.not Valid: " << data;
+        }
+        painter->restore();
+
+        if (0){
+            // const QModelIndex itm;// = index;
+            // // QModelIndex itm = index;
+            // // QMap<int, QVariant> rolesData;
+            // // rolesData[Qt::DisplayRole] = "";
+            // // itm.model()->setItemData(itm, rolesData);
+            // // qDebug() << "option:" << option.rect << " text:" << option.text;
+            // qDebug() << " text:" << index.data(Qt::DisplayRole) ;//<< " new:" << itm.data(Qt::DisplayRole);
+            // QStyledItemDelegate::paint(painter, option, itm); //this will draw all include text string
+            // Assuming you want to display images in column::dir
+            QVariant data = index.data(); // Retrieve data associated with the index
+            if (data.isValid()) {
+                QString imagePath = ":/"+data.toString();// Assumes you store the image path in Qt::UserRole
+                QImage image(imagePath);
+                if (!image.isNull()) {
+                    QPixmap pixmap = QPixmap::fromImage(image);
+                    QRect rect = option.rect;
+                    rect.setWidth(option.rect.width()/2);
+                    painter->drawPixmap(rect, pixmap);
+                    // painter->drawPixmap(option.rect, pixmap);
+                }else{
+                    //                qDebug() << "No image: " << imagePath << ::Qt::endl;
+                }
+            } else {
+                qDebug() << "data.not Valid: " << data;
+            }
         }
     }
     else {
