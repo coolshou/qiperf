2aff8c8f675f778116cbbad5d01f198628bac859
coolshou <jimmy_yeh@alphanetworks.com>
2025-02-26 09:25:25 +0800

    add echo

--- build_x64.bat
+++ build_x64.bat
@@ -8,7 +8,9 @@ REM del /Q /S qiperfc_x86_64\*
 REM del /Q /S qiperfd_x86_64\*
 REM del /Q /S qiperftray_x86_64\*
 
+echo qmake...
 qmake
+echo nmake...
 nmake
 
 set PATH=C:\Program Files (x86)\NSIS\;%PATH%
@@ -17,5 +19,7 @@ set APPVERSION=0.8
 set QIPERFD_FileVersion=0.8.11402.25
 set QIPERFC_FileVersion=0.8.11402.25
 
+echo create qiperf daemon setup...
 makensis.exe /DAPPVERSION=%APPVERSION% /DAPPFileVersion=%QIPERFD_FileVersion% /DWIN64 /V4 qiperfd.nsi
+echo create qiperf setup...
 makensis.exe /DAPPVERSION=%APPVERSION% /DAPPFileVersion=%QIPERFC_FileVersion% /DWIN64 /V4 qiperf.nsi
