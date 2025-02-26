2a541c9ce4fc74e6c6acb039305d39bf135fdd25
coolshou <jimmy_yeh@alphanetworks.com>
2025-02-26 09:44:17 +0800

    update build_x64

--- build_x64.bat
+++ build_x64.bat
@@ -1,23 +1,59 @@
 @ECHO OFF
+setlocal enabledelayedexpansion
 
-REM vc 2022 Community
-%comspec% /k "C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvars64.bat"
+REM # get version from version.h
+set "file=src\versions.h"
+set "qiperf_pattern=#define QIPERF_VERSION"
+set "qiperfd_pattern=#define QIPERFD_VERSION"
+set "qiperfc_pattern=#define QIPERFC_VERSION"
 
-REM nmake  distclean
-REM del /Q /S qiperfc_x86_64\*
-REM del /Q /S qiperfd_x86_64\*
-REM del /Q /S qiperftray_x86_64\*
+for /f "tokens=2 delims=\" %%i in ('findstr /c:"%qiperf_pattern%" "%file%"') do (
+    set "APPVERSION=%%i"
+    echo !APPVERSION!
+)
+for /f "tokens=2 delims=\" %%i in ('findstr /c:"%qiperfd_pattern%" "%file%"') do (
+    set "QIPERFD_FileVersion=%%i"
+    echo !QIPERFD_FileVersion!
+)
+for /f "tokens=2 delims=\" %%i in ('findstr /c:"%qiperfc_pattern%" "%file%"') do (
+    set "QIPERFC_FileVersion=%%i"
+    echo !QIPERFC_FileVersion!
+)
 
-echo qmake...
-qmake
-echo nmake...
-nmake
+set BUILDAPP=0
+if "%1"=="setup" (
+    echo Argument 1 is setup
+) else (
+    set BUILDAPP=1
+)
 
-set PATH=C:\Program Files (x86)\NSIS\;%PATH%
-REM # TODO, get version from version.h
-set APPVERSION=0.8
-set QIPERFD_FileVersion=0.8.11402.25
-set QIPERFC_FileVersion=0.8.11402.25
+if "%BUILDAPP%"=="1" (
+    echo %PATH% | findstr /c:"Visual Studio\2022\Community" >nul
+    if %errorlevel% equ 0 (
+        echo "Visual Studio 2022 Community" is in the PATH
+    ) else (
+        echo set Visual Studio 2022 Community in PATH
+        %comspec% /k "C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvars64.bat"
+    )
+
+    REM nmake  distclean
+    REM del /Q /S qiperfc_x86_64\*
+    REM del /Q /S qiperfd_x86_64\*
+    REM del /Q /S qiperftray_x86_64\*
+
+    echo qmake...
+    qmake
+    echo nmake...
+    nmake
+)
+
+echo %PATH% | findstr /c:"NSIS" >nul
+if %errorlevel% equ 0 (
+    echo "NSIS" is in the PATH
+) else (
+    echo set NSIS PATH
+    set PATH=C:\Program Files (x86)\NSIS\;%PATH%
+)
 
 echo create qiperf daemon setup...
 makensis.exe /DAPPVERSION=%APPVERSION% /DAPPFileVersion=%QIPERFD_FileVersion% /DWIN64 /V4 qiperfd.nsi
