02486f8e2b0d10d1a24f2c1c29f9e15077cbd2f0
coolshou <jimmy_yeh@alphanetworks.com>
2025-03-03 16:29:21 +0800

    test

--- build_x64.bat
+++ build_x64.bat
@@ -59,6 +59,8 @@ for /f "tokens=2 delims=\" %%i in ('findstr /c:"%qiperfc_pattern%" "%file%"') do
 )
 
 echo create qiperf daemon setup...
+echo "makensis.exe /DAPPVERSION=%APPVERSION% /DAPPFileVersion=%QIPERFD_FileVersion% /DWIN64 /V4 qiperfd.nsi"
 makensis.exe /DAPPVERSION=%APPVERSION% /DAPPFileVersion=%QIPERFD_FileVersion% /DWIN64 /V4 qiperfd.nsi
 echo create qiperf setup...
+echo "makensis.exe /DAPPVERSION=%APPVERSION% /DAPPFileVersion=%QIPERFC_FileVersion% /DWIN64 /V4 qiperf.nsi"
 makensis.exe /DAPPVERSION=%APPVERSION% /DAPPFileVersion=%QIPERFC_FileVersion% /DWIN64 /V4 qiperf.nsi
