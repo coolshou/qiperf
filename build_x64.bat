d95e3ee6c5956667c10c20eb277fdce97802d1d6
coolshou <jimmy_yeh@alphanetworks.com>
2025-03-10 08:57:11 +0800

    test

--- build_x64.bat
+++ build_x64.bat
@@ -8,14 +8,14 @@ if "%1"=="setup" (
 )
 
 if "%BUILDAPP%"=="1" (
-    echo "%PATH%" | findstr /c:"Qt" >nul
+    echo "%PATH%" | findstr /c:"Qt"
     if %errorlevel% EQU 1 (
         set currentDir=%cd%
         echo set QT msvc2022_64 PATH
         %comspec% /A /Q /K "C:\Qt\6.8.0\msvc2022_64\bin\qtenv2.bat"
         cd %currentDir%
     )
-    echo "%PATH%" | findstr /c:"Visual Studio" >nul
+    echo "%PATH%" | findstr /c:"Visual Studio"
     if %errorlevel% EQU 1 (
         echo set Visual Studio 2022 Community PATH
         %comspec% /k "C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvars64.bat"
