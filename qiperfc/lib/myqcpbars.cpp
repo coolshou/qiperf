64ac3ef0f821a90c2223fe986315f47398e6243a
coolshou <jimmy_yeh@alphanetworks.com>
2025-03-25 16:39:25 +0800

    add getValue, updateValue

--- qiperfc/lib/myqcpbars.cpp
+++ qiperfc/lib/myqcpbars.cpp
@@ -11,3 +11,26 @@ void MyQCPBars::addData(double key, double value)
     QCPBars::addData(key, value);
     emit dataAdded(key, value);
 }
+
+int MyQCPBars::getValue(double key, double &value)
+{
+    for (int i=this->dataCount()-1; i>0; i--){
+        if (this->data()->at(i)->key == key){
+            value = this->data()->at(i)->value;
+            return i;
+        }
+    }
+    return -1;
+}
+
+void MyQCPBars::updateValue(double keyToUpdate, double newvalue)
+{
+    QSharedPointer<QCPBarsDataContainer> dataContainer = data();
+    // Iterate over the data points to find the specific key
+    for (auto it = dataContainer->begin(); it != dataContainer->end(); ++it) {
+        if (qFuzzyCompare(it->key, keyToUpdate)) { // Check if the key matches
+            it->value = newvalue; // Update the value
+            break;
+        }
+    }
+}
--- qiperfc/lib/myqcpbars.h
+++ qiperfc/lib/myqcpbars.h
@@ -11,6 +11,8 @@ class MyQCPBars : public QCPBars
 public:
     MyQCPBars(QCPAxis *keyAxis, QCPAxis *valueAxis);
     void addData(double key, double value);
+    int getValue(double key, double &value);
+    void updateValue(double keyToUpdate, double newvalue);
 signals:
     void dataAdded(double key, double value);
 
