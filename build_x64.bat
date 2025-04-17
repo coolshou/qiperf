4671143e9f1a22c2838a893c0f9dc0c9611b4f71
coolshou <jimmy_yeh@alphanetworks.com>
2025-04-17 14:40:29 +0800

    update QT path to 6.8.3

--- build_x64.bat
+++ build_x64.bat
@@ -15,14 +15,15 @@ if "%BUILDAPP%"=="1" (
         set currentDir=%cd%
         set "driveLetter=%currentDir:~0,2%"
         echo set QT msvc2022_64 PATH
-        %comspec% /k "C:\Qt\6.8.0\msvc2022_64\bin\qtenv2.bat"
+        REM this will enter another comspec!
+        %comspec% /k "C:\Qt\6.8.3\msvc2022_64\bin\qtenv2.bat"
         %driveLetter%
     )
     echo "Check Visual Studio 2022 build environment"
     echo "%PATH%" | findstr /c:"Visual Studio"
     if !errorlevel! neq 0 (
         echo set Visual Studio 2022 Community PATH
-        %comspec% /k "C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvars64.bat"
+        %comspec% /k "C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvarsall.bat" x64
     )
 
     REM nmake  distclean
