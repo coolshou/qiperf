2b8612edae435b30e95f30e0f1fa1075fd0e28f9
coolshou <jimmy_yeh@alphanetworks.com>
2025-04-21 11:08:48 +0800

    test env

--- build_x64.bat
+++ build_x64.bat
@@ -12,7 +12,7 @@ if "%BUILDAPP%"=="1" (
     echo "Check Visual Studio 2022 build environment"
     echo "%PATH%" | findstr /c:"Visual Studio"
     if !errorlevel! neq 0 (
-        echo set Visual Studio 2022 Community PATH
+        echo set Visual Studio 2022 Community PATH "C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvarsall.bat" x64
         "C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvarsall.bat" x64
     )
 
@@ -20,7 +20,7 @@ if "%BUILDAPP%"=="1" (
     echo "%PATH%" | findstr /c:"Qt"
     if !errorlevel! neq 0 (
         set currentDir=%cd%
-        set "driveLetter=%currentDir:~0,2%"
+        set driveLetter=%currentDir:~0,2%
         echo set QT msvc2022_64 PATH
         REM this will enter another comspec!
         "C:\Qt\6.8.3\msvc2022_64\bin\qtenv2.bat"
