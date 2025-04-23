90dbc0ec6f2f7d0b5ba5cdf7ed45139aa2dd6d8f
coolshou <jimmy_yeh@alphanetworks.com>
2025-04-23 17:19:03 +0800

    remove -nc

--- build_deb.sh
+++ build_deb.sh
@@ -18,6 +18,6 @@ for package in ${BUILDPACKAGES[@]} ; do
     fi
     cd $package
     #debuild -b -uc -us
-    dpkg-buildpackage -b --no-sign -nc
+    dpkg-buildpackage -b --no-sign
     cd ..
 done
