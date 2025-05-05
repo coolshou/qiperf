878af07fa44ccb5ea048fd0ee66ed0aa02bad259
coolshou <jimmy_yeh@alphanetworks.com>
2025-05-05 16:47:27 +0800

    add menu and change to QTableWidget

--- src/gps/dlggpscalc.cpp
+++ src/gps/dlggpscalc.cpp
@@ -3,14 +3,26 @@
 
 #include "gpsfunc.h"
 
+#include <QTableWidgetItem>
+#include <QMessageBox>
+
+#include <QDebug>
+
+
 DlgGpsCalc::DlgGpsCalc(QWidget *parent) :
     QDialog(parent),
     ui(new Ui::DlgGpsCalc)
 {
     ui->setupUi(this);
+    ui->pbTaipei101SkyTree->setVisible(false);
+    initAction();
     connect(ui->pbTaipei101SkyTree, &QPushButton::clicked, this, &DlgGpsCalc::onTaipei101SkyTree);
     connect(ui->pbCalc, &QPushButton::clicked, this, &DlgGpsCalc::onCalcCliecked);
     connect(ui->pbPos, &QPushButton::clicked, this, &DlgGpsCalc::onPosCliecked);
+    connect(ui->pbClear, &QPushButton::clicked, m_clearAction, &QAction::triggered);
+    connect(ui->tableWidget, &QTableWidget::customContextMenuRequested,
+            this, &DlgGpsCalc::showContextMenu);
+
 }
 
 DlgGpsCalc::~DlgGpsCalc()
@@ -30,29 +42,122 @@ void DlgGpsCalc::changeEvent(QEvent *e)
     }
 }
 
+void DlgGpsCalc::initAction()
+{
+    m_contextMenu = new QMenu(this);
+
+    m_insertAction = m_contextMenu->addAction("Insert");
+    connect(m_insertAction, &QAction::triggered, this , &DlgGpsCalc::onInsert);
+    m_deleteAction = m_contextMenu->addAction("Delete");
+    connect(m_deleteAction, &QAction::triggered, this , &DlgGpsCalc::onDelete);
+    m_clearAction = new QAction("clear");
+        // m_contextMenu->addAction("clear");
+    connect(m_clearAction, &QAction::triggered, this , &DlgGpsCalc::onClear);
+}
+
+void DlgGpsCalc::onInsert(bool checked)
+{
+    Q_UNUSED(checked)
+    int iRow = ui->tableWidget->rowCount();
+    ui->tableWidget->setRowCount(iRow+1);
+    ui->tableWidget->setItem(iRow, 0, new QTableWidgetItem("Pos" + QString::number(iRow)));
+
+}
+
+void DlgGpsCalc::onDelete(bool checked)
+{
+    Q_UNUSED(checked)
+    int iRow = ui->tableWidget->currentRow();//->selectRow();
+    qDebug() << "onDelete:" <<  QString::number(iRow);
+     ui->tableWidget->removeRow(iRow);
+}
+
+void DlgGpsCalc::onClear(bool checked)
+{
+    Q_UNUSED(checked)
+    //TODO: ask before clear
+    if (ui->tableWidget->rowCount()>0){
+        ui->tableWidget->clearContents();
+        ui->tableWidget->setRowCount(0);
+    }
+}
+
 void DlgGpsCalc::onTaipei101SkyTree(bool checked)
 {
+    Q_UNUSED(checked)
     // 台北 101 大樓座標
     double lat1 = 25.033964;
     double lon1 = 121.564472;
-    ui->lat1->setText(QString::number(lat1));
-    ui->lon1->setText(QString::number(lon1));
+    // ui->lat1->setText(QString::number(lat1));
+    // ui->lon1->setText(QString::number(lon1));
     // 東京晴空塔座標
     double lat2 = 35.710046;
     double lon2 = 139.810718;
-    ui->lat2->setText(QString::number(lat2));
-    ui->lon2->setText(QString::number(lon2));
+    // ui->lat2->setText(QString::number(lat2));
+    // ui->lon2->setText(QString::number(lon2));
+
+    ui->tableWidget->clearContents();
+    // int irow = ui->tableWidget->currentRow();
+    // ui->tableWidget->insertRow(irow+1);
+    ui->tableWidget->setRowCount(2);
+
+    ui->tableWidget->setItem(0,0, new QTableWidgetItem("taipei101"));
+    ui->tableWidget->setItem(0,1, new QTableWidgetItem(QString::number(lat1)));
+    ui->tableWidget->setItem(0,2, new QTableWidgetItem(QString::number(lon1)));
+    ui->tableWidget->setItem(1,0, new QTableWidgetItem("SkyTree"));
+    ui->tableWidget->setItem(1,1, new QTableWidgetItem(QString::number(lat2)));
+    ui->tableWidget->setItem(1,2, new QTableWidgetItem(QString::number(lon2)));
 
 }
 void DlgGpsCalc::onCalcCliecked(bool checked)
 {
     Q_UNUSED(checked)
+    int iRow = ui->tableWidget->rowCount();
+    if (iRow<2){
+        QMessageBox::warning(this, tr("WARNING!!"),
+                             tr("Please add at last two GPS location record"),
+                             QMessageBox::Ok);
+        return;
+    }
+    QString pos1 = ui->tableWidget->item(0,0)->text();
+    double lat1 = ui->tableWidget->item(0,1)->text().toDouble();
+    double lon1 = ui->tableWidget->item(0,2)->text().toDouble();
+    QString pos = "";
+    double lat=0.0;
+    double lon=0.0;
+    double distance = 0;
+    double azimuth = 0;
+    double azimuth2 = 0;
+    ui->twResult->setRowCount(iRow-1);
+
+    for (int i=1; i<ui->tableWidget->rowCount(); i++){
+        pos = ui->tableWidget->item(i,0)->text();
+        lat = ui->tableWidget->item(i,1)->text().toDouble();
+        lon = ui->tableWidget->item(i,2)->text().toDouble();
+        if (ui->rbVincenty->isChecked()){
+            VincentyResult vrs = vincentyInverse(lat1 , lon1, lat, lon);
+            distance = vrs.distance/1000; // m -> KM
+            azimuth = vrs.initialBearing;
+            azimuth2 = vrs.finalBearing;
+        }
+        if (ui->rbHaversine->isChecked()){
+            distance = haversine(lat1 , lon1, lat, lon);
+            azimuth = calcBearing(lat1 , lon1, lat, lon);
+            azimuth2 = calcBearing(lat, lon, lat1 , lon1);
+        }
+        ui->twResult->setItem(i-1, 0, new QTableWidgetItem(pos1 + " : " + pos));
+        ui->twResult->setItem(i-1, 1, new QTableWidgetItem(QString::number(distance)));
+        ui->twResult->setItem(i-1, 2, new QTableWidgetItem(QString::number(azimuth)));
+        ui->twResult->setItem(i-1, 3, new QTableWidgetItem(QString::number(azimuth2)));
+    }
+    /*
     if (ui->lat1->text().isEmpty()){
         return;
     }
     // TODO: check lat1/lon1/lat2/lon2 format
-    // DD.mm.ss.sssss N/S/E/W
-    // DD.mm.mmmmm N/S/E/W
+    // DD°mm'ss.sssss" N/S/E/W
+    // DD°mm.mmmmm' N/S/E/W
+    // DD.DDDDD°
     //
     double lat1 = ui->lat1->text().toDouble();
     double lon1 = ui->lon1->text().toDouble();
@@ -77,11 +182,25 @@ void DlgGpsCalc::onCalcCliecked(bool checked)
     ui->azimuthGPS12->setText(QString::number(azimuth));
     ui->azimuthGPS21->setText(QString::number(azimuth2));
     ui->leDistance->setText(QString::number(distance));
+*/
 
 }
 
 void DlgGpsCalc::onPosCliecked(bool checked)
 {
     Q_UNUSED(checked)
-    // show google map
+    qDebug() << "//TODO show google map" ;
+}
+
+void DlgGpsCalc::showContextMenu(const QPoint &pos)
+{
+    if (ui->tableWidget->rowCount()<1){
+        m_deleteAction->setEnabled(false);
+        m_deleteAction->setVisible(false);
+    }else {
+        m_deleteAction->setEnabled(true);
+        m_deleteAction->setVisible(true);
+    }
+
+    m_contextMenu->exec(mapToGlobal(pos));
 }
--- src/gps/dlggpscalc.h
+++ src/gps/dlggpscalc.h
@@ -2,6 +2,9 @@
 #define DLGGPSCALC_H
 
 #include <QDialog>
+#include <QPoint>
+#include <QMenu>
+#include <QAction>
 
 namespace Ui {
 class DlgGpsCalc;
@@ -19,12 +22,21 @@ protected:
     void changeEvent(QEvent *e);
 
 private slots:
+    void initAction();
+    void onInsert(bool checked);
+    void onDelete(bool checked);
+    void onClear(bool checked);
     void onTaipei101SkyTree(bool checked);
     void onCalcCliecked(bool checked);
     void onPosCliecked(bool checked);
 
+    void showContextMenu(const QPoint &pos);
 private:
     Ui::DlgGpsCalc *ui;
+    QMenu *m_contextMenu;
+    QAction *m_insertAction;
+    QAction *m_deleteAction;
+    QAction *m_clearAction;
 };
 
 #endif // DLGGPSCALC_H
