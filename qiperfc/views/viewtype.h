6daf0f67649b1428272fa6eba554ee99e141e31e
coolshou <jimmy_yeh@alphanetworks.com>
2025-05-15 15:10:49 +0800

    add ViewType

---
+++ qiperfc/views/viewtype.h
@@ -0,0 +1,16 @@
+#ifndef VIEWTYPE_H
+#define VIEWTYPE_H
+
+
+enum ViewType{
+    Throughput,
+    Serial,
+    SSH,
+    Telnet,
+    Ping,
+    WlanSTA,
+    WebControl
+};
+
+
+#endif // VIEWTYPE_H
