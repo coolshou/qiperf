0daf25cd484ea75e632355a06a854eefa29d6a20
coolshou <jimmy_yeh@alphanetworks.com>
2025-05-21 09:52:59 +0800

    add libQSsh.a check

--- build_deb.sh
+++ build_deb.sh
@@ -11,7 +11,11 @@ export QT_SELECT=qt5
 fi
 
 rm -f *.buildinfo *.changes *.deb
-
+if [ ! -e lib/qssh/lib/libQSsh.a ]; then
+  cd lib/qssh
+  dpkg-buildpackage -b --no-sign
+  cd -
+fi
 for package in ${BUILDPACKAGES[@]} ; do
     if [ ! -e $package/debian/changelog ]; then
         ln -s debian/changelog $package/debian/
