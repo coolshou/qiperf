8695d4e42b73eb755df909c3bfe242d0a290ba5c
coolshou <jimmy_yeh@alphanetworks.com>
2025-05-22 12:37:01 +0800

    try defined

--- src/gps/geotranslate.cpp
+++ src/gps/geotranslate.cpp
@@ -111,9 +111,9 @@ double GeoTranslate::convertEllipsoidToMSL(double lat, double lon, double ellips
     try {
         // const GeographicLib::Geoid& geoid = GeographicLib::Geoid::EGM96(); // or EGM2008
         // Construct with the geoid model name (must be installed!)
-#ifdef Q_OS_LINUX
+#ifdef defined(Q_OS_LINUX)
         QString geodatapath="/opt/qiperf/bin/geoids";
-#elif Q_OS_WINDOWS
+#elif defined(Q_OS_WINDOWS)
         QString geodatapath=qApp->applicationDirPath() + QDir::separator() + "geoids";
 #else
         qDebug() << "Not support platform";
