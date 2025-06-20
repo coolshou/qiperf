ccbca09b7ff09aff6ec8914ea8eb56f6540c2856
coolshou <coolshou2060@gmail.com>
2025-06-20 19:45:08 +0800

    add TPStatus

---
+++ qiperfc/src/tpstatus.h
@@ -0,0 +1,21 @@
+#ifndef TPSTATUS_H
+#define TPSTATUS_H
+
+#include <QObject>
+
+class TPStatus: public QObject
+{
+    Q_GADGET
+public:
+    enum Status{
+        init=0,  // init
+        started=1,  // started
+        stoped=2     // stoped
+    };
+    // init=-1,  // init
+    //     started=0,  // started
+    //     stoped=1     // stoped
+    Q_ENUM(Status)
+};
+
+#endif // TPSTATUS_H
