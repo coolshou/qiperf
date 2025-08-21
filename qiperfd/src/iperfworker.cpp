fb88ee08a24a7e303154868a04c342229094b9c3
coolshou <jimmy_yeh@alphanetworks.com>
2025-08-21 12:00:21 +0800

    debug

--- qiperfd/src/iperfworker.cpp
+++ qiperfd/src/iperfworker.cpp
@@ -271,7 +271,7 @@ void IperfWorker::setStop()
                 // }
             }
         }else {
-            debug("m_iperf state:"+ QString::number(m_iperf->state()), 1);
+            debug("m_iperf is not running, state:"+ QString::number(m_iperf->state()), 1);
         }
     }
     emit workerFinished(m_idx, m_servermode); // Notify manager that this worker is logically done
