4bb44853ca3c25f7fbf2b7c786404b4749838002
coolshou <jimmy_yeh@alphanetworks.com>
2026-03-31 14:53:21 +0800

    fix Qt path

--- build_x64.bat
+++ build_x64.bat
@@ -16,8 +16,8 @@ if "%BUILDAPP%"=="1" (
         set driveLetter=%currentDir:~0,2%
         echo set QT msvc2022_64 PATH
         REM this will enter another comspec!
-        echo "C:\Qt\6.8.3\msvc2022_64\bin\qtenv2.bat"
-        "C:\Qt\6.8.3\msvc2022_64\bin\qtenv2.bat"
+        echo "C:\Qt\6.10.1\msvc2022_64\bin\qtenv2.bat"
+        "C:\Qt\6.10.1\msvc2022_64\bin\qtenv2.bat"
         echo "back to %driveLetter%"
         %driveLetter%
     )
