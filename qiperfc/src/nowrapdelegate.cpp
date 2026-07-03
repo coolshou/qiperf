847a4783020224ca5e425abb4f9c2810a0a26890
coolshou <coolshou2060@gmail.com>
2026-07-03 22:09:48 +0800

    fix raw hogh

--- qiperfc/src/nowrapdelegate.cpp
+++ qiperfc/src/nowrapdelegate.cpp
@@ -11,3 +11,15 @@ void NoWrapDelegate::paint(QPainter *painter, const QStyleOptionViewItem &option
     opt.textElideMode = Qt::ElideRight; // This will truncate the text with an ellipsis if it doesn't fit
     QStyledItemDelegate::paint(painter, opt, index);
 }
+
+QSize NoWrapDelegate::sizeHint(const QStyleOptionViewItem &option, const QModelIndex &index) const
+{
+    QSize size = QStyledItemDelegate::sizeHint(option, index);
+
+    // Force the height to a fixed value (e.g., 24 pixels),
+    // or base it on the font metrics for a clean single line:
+    int fontHeight = option.fontMetrics.height();
+    size.setHeight(fontHeight + 6); // Font height + 6px padding (3px top/bottom)
+
+    return size;
+}
--- qiperfc/src/nowrapdelegate.h
+++ qiperfc/src/nowrapdelegate.h
@@ -9,6 +9,7 @@ class NoWrapDelegate : public QStyledItemDelegate
 public:
     explicit NoWrapDelegate(QObject *parent = nullptr);
     void paint(QPainter *painter, const QStyleOptionViewItem &option, const QModelIndex &index) const override;
+    QSize sizeHint(const QStyleOptionViewItem &option, const QModelIndex &index) const override;
 };
 
 #endif // NOWRAPDELEGATE_H
