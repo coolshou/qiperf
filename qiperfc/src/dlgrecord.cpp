52295a6c431f7a932effd47f6663a0e970b1c918
coolshou <jimmy_yeh@alphanetworks.com>
2026-07-30 16:11:55 +0800

    fix show some file's size=0, but the file do have data

--- qiperfc/src/dlgrecord.cpp
+++ qiperfc/src/dlgrecord.cpp
@@ -18,14 +18,26 @@ DlgRecord::DlgRecord(QWidget *parent) :
     m_fileModel->setFilter(QDir::NoDotAndDotDot | QDir::Files);
     m_fileModel->setOptions(QFileSystemModel::DontResolveSymlinks|
                             QFileSystemModel::DontUseCustomDirectoryIcons);
+
+    // connect(m_fileModel, &QFileSystemModel::directoryLoaded, this, &DlgRecord::onRefresh);
+    connect(m_fileModel, &QFileSystemModel::directoryLoaded, this, [this](const QString &path){
+        if (path == m_rootpath) {
+            // Option A: Expand/update the view layout
+            ui->tvLogFiles->viewport()->update();
+        }
+    });
     // connect(m_fileModel, &QFileSystemModel::);
     ui->tvLogFiles->setModel(m_fileModel);
+    ui->tvLogFiles->setColumnWidth(0, 400);
+    ui->tvLogFiles->setColumnWidth(3, 150);
+
     connect(ui->tvLogFiles, &QTreeView::doubleClicked, this, &DlgRecord::onItemDClicked); //edit item on double click
     // TooltipEventFilter *m_filter = new TooltipEventFilter(ui->tvLogFiles);
     // connect(m_filter, &TooltipEventFilter::doRefresh, this, &DlgRecord::onRefresh);
     // ui->tvLogFiles->viewport()->installEventFilter(m_filter);
     refreshShortcut=new QShortcut(QKeySequence(Qt::Key_F5), ui->tvLogFiles);
     connect(refreshShortcut, &QShortcut::activated,this,&DlgRecord::onRefresh);
+    connect(ui->pbRefresh, &QPushButton::clicked, this, &DlgRecord::onRefresh);
 }
 
 DlgRecord::~DlgRecord()
@@ -36,11 +48,10 @@ DlgRecord::~DlgRecord()
 void DlgRecord::setRootPath(QString rootpath)
 {
     m_rootpath = rootpath;
-    m_fileModel->setRootPath(rootpath);
-    QModelIndex idx = m_fileModel->index(m_fileModel->rootPath());
-    ui->tvLogFiles->setRootIndex(idx);
-    ui->tvLogFiles->setColumnWidth(0, 400);
-    ui->tvLogFiles->setColumnWidth(3, 150);
+    onRefresh();
+    // QModelIndex rootIndex = m_fileModel->setRootPath(rootpath);
+    // // QModelIndex idx = m_fileModel->index(m_fileModel->rootPath());
+    // ui->tvLogFiles->setRootIndex(rootIndex);
     setWindowTitle(QDir::toNativeSeparators(rootpath));
 }
 
@@ -85,35 +96,35 @@ void DlgRecord::closeEvent(QCloseEvent *e)
 
 void DlgRecord::onItemDClicked(QModelIndex idx)
 {
-    if (idx.column()==0){
-        auto itm = m_fileModel->itemData(idx);
-        if (!itm.isEmpty()){
-            QString filename = m_rootpath + QDir::separator() + itm[0].toString();
-            if (!m_logfiles.contains(filename)){
-                m_logfiles[filename] = new CodeEditor();
-                connect(m_logfiles[filename], &CodeEditor::Closing, this, &DlgRecord::onClosing);
-                m_logfiles[filename]->load(filename);
-                m_logfiles[filename]->show();
-            }else{
-                m_logfiles[filename]->setWindowState((m_logfiles[filename]->windowState() & ~Qt::WindowMinimized) | Qt::WindowActive);
-                m_logfiles[filename]->raise();
-            }
-        }
+    // Check index validity and ensure click was on column 0
+    if (!idx.isValid() || idx.column() != 0)
+        return;
+
+    // Get exact absolute path directly from QFileSystemModel
+    QString filename = m_fileModel->filePath(idx);
+
+    if (!m_logfiles.contains(filename)){
+        m_logfiles[filename] = new CodeEditor();
+        connect(m_logfiles[filename], &CodeEditor::Closing, this, &DlgRecord::onClosing);
+        m_logfiles[filename]->load(filename);
+        m_logfiles[filename]->show();
+    }else{
+        m_logfiles[filename]->setWindowState((m_logfiles[filename]->windowState() & ~Qt::WindowMinimized) | Qt::WindowActive);
+        m_logfiles[filename]->raise();
     }
 }
 
 void DlgRecord::onRefresh()
 {
     qDebug() << "onRefresh(TODO, following did not update file size?why)";
-    // m_fileModel->setRootPath(m_rootpath);
-    QModelIndex currentIndex = ui->tvLogFiles->rootIndex();
-    ui->tvLogFiles->setRootIndex(m_fileModel->index(m_fileModel->filePath(currentIndex)));
+    QModelIndex rootIdx = m_fileModel->setRootPath("");
+    rootIdx = m_fileModel->setRootPath(m_rootpath);
+    ui->tvLogFiles->setRootIndex(rootIdx);
 }
 
 void DlgRecord::onBrowser(bool checked)
 {
     Q_UNUSED(checked)
-    qDebug() << "open " << m_rootpath << " in file manager";
     QProcess process;
 #if defined(Q_OS_WIN)
     QString command = "explorer";
